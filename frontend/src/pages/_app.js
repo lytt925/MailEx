cb23aac55f2d9376e1cf64de7dde57e7f8bd168f
Yen-Ting Li <ytli.tw@gmail.com>
2023-12-12 03:24:27 +0800

    fix: mui styledengine

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,7 +1,7 @@
 import { Noto_Sans_TC } from 'next/font/google';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createTheme, ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
 import { Navbar } from '@/components/Navbar'
 import { UserProvider } from '@/hooks/useUserContext'; // Assuming UserContext is the file name
 import '@/styles/globals.css'
@@ -28,15 +28,17 @@ export default function App({ Component, pageProps }) {
   return (
     <QueryClientProvider client={queryClient}>
       <UserProvider>
-        <ThemeProvider theme={theme}>
-          <main className={`flex flex-col min-h-screen ${noto.className}`}>
-            <Navbar />
-            <div className="flex justify-center items-center h-custom w-full xl:max-w-[1300px] text-lg my-0 mx-auto"> {/* w-full */}
-              <Component {...pageProps} />
-              <ReactQueryDevtools initialIsOpen={false} />
-            </div>
-          </main >
-        </ThemeProvider>
+        <StyledEngineProvider injectFirst>
+          <ThemeProvider theme={theme}>
+            <main className={`flex flex-col min-h-screen ${noto.className}`}>
+              <Navbar />
+              <div className="flex justify-center items-center h-custom w-full xl:max-w-[1300px] text-lg my-0 mx-auto"> {/* w-full */}
+                <Component {...pageProps} />
+                <ReactQueryDevtools initialIsOpen={false} />
+              </div>
+            </main >
+          </ThemeProvider>
+        </StyledEngineProvider>
       </UserProvider>
     </QueryClientProvider>
   )
